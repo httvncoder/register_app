bc58b14d57ac0e9316577943887040d8f9533d38
ArnoVDM <arnovandermerwe39@gmail.com>
2016-09-13 00:02:08 +0200

    Added Community Search and Matrix implementation
    Added local community search function by reading in a local csv file.
    Added decision making capabilities based on matrix that is
    predetermined.

--- A_Register/www/js/app.js
+++ A_Register/www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('app', ['ionic', 'pascalprecht.translate', 'ngStorage',  'app.controllers', 'app.routes', 'app.strings', 'app.services', 'app.directives'])
+angular.module('app', ['ionic', 'pascalprecht.translate', 'ngStorage', 'app.matrix',  'app.controllers', 'app.routes', 'app.strings', 'app.services', 'app.directives'])
 
 
 
---
+++ A_Register/www/js/matrix.js
@@ -0,0 +1,58 @@
+angular.module('app.matrix', [])
+
+.service('matrix', [function(){
+
+var matrix = {}
+
+//items = [terms_monitor, terms_fisher,	terms_consentbox,	termsCtrl_open_modal,	termsCtrl_from_modal,	termsCtrl_from_terms,	mfc_landingsite1,	mfc_landingsite2,	mfc_landingsite3,	mfc_co_op	personal_details_title_all,	personal_details_title_co,	personal_details_device_num,	photoCtrl_from_photo
+
+
+/*matrix that determines whether an element will show or not corresponding to a given user type or where
+the page should route to. If a item is a html element the id will correspond to name above and will be used according to its index
+If the matrix is used in the controller for routing the matrix will route according to the string listed above.
+*/
+var role_fisher =           [false,true,true,true,false,false, false, false, false, false, true, false, false, true]
+var role_fisher_manager =   [false,false,false,false,false,true, true, false, true, false, true, false, false, false]
+var role_co_op =            [false,true,true,true,true,false,false,false,false,true, false, true, true, false]
+var role_monitor =          [true, false, false,true,true,false,true,true,false,false, true, false, true, false]
+var role_monitor_manager =  [false,false,false,false,false,false,false,false,false,false, true, false, false, false]
+var role_demo_fisher =      [false,true,true,true,false,false,false,false,false,false, true, false, false, true]
+var role_demo_monitor =     [true, false, false,true,true,false,true,true,false,false, true, false, true, false]
+var role_co_op_admin =      [false,true,true,true,true,false,false,false,false,true,false, true, true, false]
+
+
+matrix.evaluate =  function (role, destination) {
+  switch (role) {
+  case 'fisher':
+      return role_fisher[destination]
+
+  case 'fisher_manager':
+      return role_fisher_manager[destination]
+
+  case 'co_op':
+      return role_co_op[destination]
+
+  case 'monitor':
+      return role_monitor[destination]
+
+  case 'monitor_manager':
+      return role_monitor_manager[destination]
+
+  case 'demo_fisher':
+      return role_demo_fisher[destination]
+
+  case 'demo_monitor':
+     return role_demo_monitor[destination]
+
+  case 'co_op_admin':
+    return role_co_op_admin[destination]
+
+
+}
+  return
+
+}
+
+return matrix
+
+}])
